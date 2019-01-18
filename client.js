416ec2dd219da6c50f0a36f098780247c6831fd5
Nigam Mehta <fornigam@gmail.com>
2019-01-18 15:24:40 +0530

    Use request-promise library

--- client.js
+++ client.js
@@ -1,11 +1,95 @@
-var request = require('request');
+//var Promise = require('bluebird');
+let request = require('request');
+let rp = require('request-promise');
 console.log('server file called');
-var array_data = ['a','b','c','d','e'];
+var array_data = ['a', 'b', 'c', 'd', 'e'];
 
 
 
-var promises = [];
-for(let i = 0; i < array_data.length; i++) {
-    var promise = request.post({url: 'http://localhost:3000',form : {key:array_data[i]}});
+// var promises = [];
+// for(let i = 0; i < array_data.length; i++) {
+//     var promise = request.post({url: 'http://localhost:3000',form : {key:array_data[i]}});
+// }
+
+// var page = 0;
+// var last_page = array_data.length;
+
+// (function loop() {
+//     if (page < last_page) {
+//         request.post({
+//             url: 'http://localhost:3000',
+//             form: 'test=' + array_data[page]
+//         }, function (error, response, body) {
+//             page++;
+//             loop();
+//         });
+//     }
+// }());
+
+
+// //Not working because we are using for loop
+// async function loopAsync() {
+//     for (let i = 0; i < array_data.length; i++) {
+//         await request.post({
+//             url: 'http://localhost:3000',
+//             form: 'test=' + array_data[i]
+//         });
+//     }
+// }
+// loopAsync();
+
+
+// async function loopAsync() {
+//     var page = 0;
+//     var last_page = array_data.length;
+//     while (page < last_page) {
+//         await request.post({
+//             url: 'http://localhost:3000',
+//             form: 'test=' + array_data[page]
+//         });
+//         page++;
+//     }
+// }
+// loopAsync();
+
+//If you not use request-promise library then you can go with it.
+// async function loopAsync() {
+//     var page = 0;
+//     var last_page = array_data.length;
+//     while (page < last_page) {
+//         await new Promise((resolve, reject) => {
+//             request.post({
+//               url: 'http://localhost:3000',
+//               form: 'test=' + array_data[page]
+//              }, function(err,httpResponse,body){
+//                if (err) reject(err);
+//                else resolve({httpResponse,body});
+//              });
+//              page++;
+//         });
+//     }
+// }
+// loopAsync()
+
+
+//with the use of request-promise library 
+async function loopAsync() {
+    var page = 0;
+    var last_page = array_data.length;
+    while (page < last_page) {
+        var options = {
+            method: 'POST',
+            uri: 'http://localhost:3000',
+            body: {
+                test: array_data[page]
+            },
+            json: true // Automatically stringifies the body to JSON
+        };
+        await rp(options);
+        page++;
+    }
 }
+loopAsync();
 
+
+ 
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
+let express = require('express');
+let bodyParser = require('body-parser')
+let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-app.post('/', function (req, res) {  
-    console.log(req.body.key)
-   
+app.post('/', function (req, res) {
+   console.log(req.body.test);
+   res.status(200).send('ok');
 });
 
 var server = app.listen(3000, function () {
